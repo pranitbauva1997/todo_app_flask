42e502fad8087c7013b3913d8823ed74e769a70c
Pranit Bauva <pranit.bauva@gmail.com>
2017-10-25 20:33:27 +0530

    add database handler to add todo to list
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, List
 
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///todo.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def index():
@@ -9,6 +18,14 @@ def index():
 @app.route('/new')
 def new():
     return render_template('new.html')
+
+@app.route('/create', methods = ['GET', 'POST'])
+def create():
+    todo_entry = List(description = request.args['description'],
+                      date = request.args['date'])
+    session.add(todo_entry)
+    session.commit()
+    return "Successfully created added to list"
 
 if __name__ == '__main__':
     app.debug = True
