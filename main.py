6066c5b8230d1b0f76bc20988ef2a6de5d616ec0
Pranit Bauva <pranit.bauva@gmail.com>
2017-10-25 15:10:10 +0530

    add basic form for TODO app
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
